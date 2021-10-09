32f59df89d427c18e5ba20bc40066f31061ba306
LiyaNorng <Liya_Norng@yahoo.com>
2021-10-08 21:26:46 -0700

    Add files via upload
    Creating task and deleting task.

---
+++ CreateAndDeleteTask/src/main.cpp
@@ -0,0 +1,81 @@
+#include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
+#include <stdlib.h>
+
+static int rate = 1000;
+static const int led = LED_BUILTIN;
+static const uint8_t buf_len = 20;
+TaskHandle_t task1;
+TaskHandle_t task2;
+
+void readSerial(void *parameters) {
+
+  char c;
+  char buf[buf_len];
+  uint8_t idx = 0;
+  int temp;
+  // Clear whole buffer
+  memset(buf, 0, buf_len);
+
+  // Loop forever
+  while (1) {
+
+    // Read characters from serial
+    if (Serial.available() > 0) {
+      c = Serial.read();
+
+      // Update delay variable and reset buffer if we get a newline character
+      if (c == '\n') {
+        temp = atoi(buf);
+        Serial.print("Updated LED delay to: ");
+        Serial.println(rate);
+        if(temp < 0){
+          Serial.print("No longer taking ORDER FROM YOU. YOUR LED SETTING IS SET AT YOUR LAST SETTING.");
+          vTaskDelete(task2);
+        }
+        rate = temp;
+        memset(buf, 0, buf_len);
+        idx = 0;
+      } else {
+        
+        // Only append if index is not over message limit
+        if (idx < buf_len - 1) {
+          buf[idx] = c;
+          idx++;
+        }
+      }
+    }
+  }
+}
+
+void settingLedRate(void * id){
+
+    while(1){
+      digitalWrite(led, LOW);
+      Serial.println("Led turn OFF");
+      vTaskDelay(rate/portTICK_PERIOD_MS);
+      digitalWrite(led, HIGH);
+      Serial.println("Led turn ON");
+      vTaskDelay(rate/portTICK_PERIOD_MS);
+
+    }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  delay(3000);
+  Serial.println("Multi-task LED Demo");
+  Serial.println("Enter a number in milliseconds to change the LED delay.");
+  Serial.flush();
+
+  xTaskCreate(settingLedRate, "Setting the rate for LED", 1024, NULL, 1, &task1);
+  xTaskCreate(readSerial, "Getting input From User", 1024, NULL, 1, &task2);
+  vTaskStartScheduler();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
