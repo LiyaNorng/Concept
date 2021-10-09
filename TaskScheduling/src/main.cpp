23129e52f838177af777fb12b829f58aa14f0c24
LiyaNorng <Liya_Norng@yahoo.com>
2021-10-08 21:08:02 -0700

    Update main.cpp
    fixed some bugs

--- TaskScheduling/src/main.cpp
+++ TaskScheduling/src/main.cpp
@@ -1,32 +1,48 @@
 #include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
+#include <stdlib.h>
 
-
-/**
- * Date: October 4, 2021
- * Author: Liya Norng
- * 
- * Using mega 2560 board using freertos create two tasks. One to get user input to store for the new delay rate. 
- *  Other task is to delay the LED to that new rate from User input
- *  
- */
-
-static int rate = 8000;
+static int rate = 1000;
 static const int led = LED_BUILTIN;
+static const uint8_t buf_len = 20;
 
-void gettingInputFromUser(void * id){
-    int temp = 0;
-    while(1){
-      temp = Serial.read();
-      if(temp > 0){
-        rate = temp;
+void readSerial(void *parameters) {
+
+  char c;
+  char buf[buf_len];
+  uint8_t idx = 0;
+
+  // Clear whole buffer
+  memset(buf, 0, buf_len);
+
+  // Loop forever
+  while (1) {
+
+    // Read characters from serial
+    if (Serial.available() > 0) {
+      c = Serial.read();
+
+      // Update delay variable and reset buffer if we get a newline character
+      if (c == '\n') {
+        rate = atoi(buf);
+        Serial.print("Updated LED delay to: ");
         Serial.println(rate);
+        memset(buf, 0, buf_len);
+        idx = 0;
+      } else {
+        
+        // Only append if index is not over message limit
+        if (idx < buf_len - 1) {
+          buf[idx] = c;
+          idx++;
+        }
       }
     }
-
+  }
 }
 
 void settingLedRate(void * id){
+
     while(1){
       digitalWrite(led, LOW);
       Serial.println("Led turn OFF");
@@ -42,11 +58,17 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(led, OUTPUT);
+  delay(3000);
+  Serial.println("Multi-task LED Demo");
+  Serial.println("Enter a number in milliseconds to change the LED delay.");
+  Serial.flush();
+
   xTaskCreate(settingLedRate, "Setting the rate for LED", 1024, NULL, 1, NULL);
-  xTaskCreate(gettingInputFromUser, "Getting input From User", 1024, NULL, 1, NULL);
-  vTaskDelete(NULL);
+  xTaskCreate(readSerial, "Getting input From User", 1024, NULL, 1, NULL);
+  vTaskStartScheduler();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
 }
